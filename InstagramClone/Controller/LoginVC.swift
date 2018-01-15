3a01f6738f4db8da33c8fd116f1c7c150ef2eda4
Berkat Bhatti <beme@theknightmarket.com>
2018-01-15 07:53:48 -0500

    Login and signup completed

---
+++ InstagramClone/Controller/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  InstagramClone
+//
+//  Created by berkat bhatti on 1/15/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ InstagramClone/Controller/SignUpVC.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpVC.swift
+//  InstagramClone
+//
+//  Created by berkat bhatti on 1/15/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func alreadyHavePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
